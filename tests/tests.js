1951dc23b060524eaf5e4968ffc44597d672adbd
Gonzalo J. García Martín <=>
2014-02-20 17:36:09 +0000

    MAstest anyadidos

--- tests/tests.js
+++ tests/tests.js
@@ -6,13 +6,24 @@ suite('Fichero INI', function() {
 		  var s = "[Cabecera]";
 		  var t = lexer(s);
         assert.equal(t[0].type, 'header');
+		  assert.equal(t[0].match[1], 'Cabecera');
     });
      test(';Comentario', function() {
          var s = ";Comentario";
 		   var t = lexer(s);
-			var c = t[0].match
+			var c = t[0].match;
          assert.equal(c[0], ';Comentario');
+			assert.equal(c[1], 'Comentario');
      });
+	  test('Nombre = Valor', function() {
+		  var s = "Nombre = Gonzalo";
+		  var t = lexer(s);
+		  var m = t[0].match;
+		  assert.equal(t[0].type, 'nameEqualValue');
+		  assert.equal(m[0], 'Nombre = Gonzalo');
+		  assert.equal(m[1], 'Nombre ');
+		  assert.equal(m[2], ' Gonzalo');
+	  });
      test('Error!', function() {
 		   var s = "Esto es un error";
 		   var t = lexer(s);
