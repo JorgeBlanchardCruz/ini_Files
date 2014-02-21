144658e10a67b1d1eb83d4908b60f9af2c84e816
Gonzalo J. García Martín <=>
2014-02-21 19:25:06 +0000

    add InnerHTML

--- scripts/ini.js
+++ scripts/ini.js
@@ -12,14 +12,17 @@ window.onload = function() {
  	// If the browser supports localStorage and we have some stored data
  	if (window.localStorage){
  		if (localStorage.fileAttributes)
- 			document.getElementById("txtfileAttributes").innerHTML = localStorage.fileAttributes;
+ 			//document.getElementById("txtfileAttributes").innerHTML = localStorage.fileAttributes;
+			txtfileAttributes.innerHTML = localStorage.fileAttributes;
  			
  		if (localStorage.initialinput)	
- 			document.getElementById("initialinput").innerHTML = localStorage.initialinput;
+ 			//document.getElementById("initialinput").innerHTML = localStorage.initialinput;
+			initialinput.innerHTML = localStorage.initialinput;
  		
  		if (localStorage.finaloutput){
- 			document.getElementById("finaloutput").innerHTML = localStorage.finaloutput;
+ 			//document.getElementById("finaloutput").innerHTML = localStorage.finaloutput;
  			out.className = 'unhidden';
+			finaloutput.innerHTML = localStorage.finaloutput;
  		}		
  	}
 };
